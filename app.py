b042e6801ec841fedcaa152e356560a2f6336a3b
manickakrish <104718782+manickakrish@users.noreply.github.com>
2022-05-16 23:37:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET"])
 def index():
-       return ("Hello Coimbatore")
+       return ("Hello Delhi")
                      
 if __name__ == "__main__":
   app.run(debug=True)
